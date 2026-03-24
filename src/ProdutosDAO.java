938ee2592a00443ad4563c7ef8eed16d58221273
Victória Lima <victoriajhaniny@icloud.com>
2026-03-23 22:50:54 -0300

    Funcionalidade de cadastrar produto no banco implementada

---
+++ src/ProdutosDAO.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class ProdutosDAO {
+    
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public void cadastrarProduto (ProdutosDTO produto){
+
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+
+            prep = conn.prepareStatement(sql);
+
+            prep.setString(1, produto.getNome());
+            prep.setDouble(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+        }
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        
+        return listagem;
+    }
+}
